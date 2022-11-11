2dd7778db02ea04137b3b9d023dd1d2eb8836abb
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-12 01:45:19 +0700

    Add singleton

--- plugin.gd
+++ plugin.gd
@@ -12,8 +12,12 @@ func _enter_tree():
 		if ProjectSettings[setting] == null:
 			ProjectSettings.set_setting(setting, settings[setting])
 			ProjectSettings.set_initial_value(setting, settings[setting])
+	
+	add_autoload_singleton("GameJolt", "res://addons/atirut.gj-api/api.gd")
 
 
 func _exit_tree():
 	for setting in settings:
 		ProjectSettings.set_setting(setting, null)
+	
+	remove_autoload_singleton("GameJolt")
